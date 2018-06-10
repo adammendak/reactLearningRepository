4078520d0688acee32be64522685777e8a1d3400
adammendak <adam.mendak@gmail.com>
2018-06-10 14:46:30 +0200

    more on routing

--- router/router.jsx
+++ router/router.jsx
@@ -8,23 +8,67 @@ import {
     NavLink,
 } from 'react-router-dom';
 
-class Main extends React.Component { render() {
-    return <h1>Hello, World!</h1>; }
+class Main extends React.Component {
+    render() {
+        return(
+        <div>
+            <h1>Hello, World!</h1>
+            <Link to="/contact">Go to contact</Link>
+        </div>
+        )
+    }
 }
 
 class Contact extends React.Component {
     render() {
-        return <h1>Contact us at contact@example.com</h1>;
+        return (
+        <div>
+            <h1>Contact us at contact@example.com</h1>
+            <Link to="/">Home</Link>
+        </div>
+        )
     }
+}
+
+class NotFound extends React.Component {
+    render() {
+        return <h1>404 : not found</h1>
+    }
+}
+
+class Navigation extends React.Component {
+    render() {
+    return (
+        <div>
+            <h1>App</h1>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link> </li>
+                <li>
+                    <Link to="/contact">Contact</Link>
+                </li>
+            </ul>
+        </div>
+    )}
 }
 
 class App extends React.Component {
     render() {
-        return <Main />;
+        return (
+        <div>
+            <HashRouter>
+                <Switch>
+                    <Route exact path='/' component={Main} />
+                    <Route path='/contact' component={Contact} />
+                    <Route component={NotFound} />
+                </Switch>
+            </HashRouter>
+        </div>
+        )
     }
 }
 
-ReactDOM.render(
+    ReactDOM.render(
     <App />,
     document.getElementById('app')
 );
